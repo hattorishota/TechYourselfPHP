e1adc4eb5338e7943bea056160abd2b7df27b7b3
hattorishota <hattorishota.eng@gmail.com>
2022-06-17 01:51:06 +0900

    Edit sec2-1.php ・変数の宣言 ・可変変数 →$$と重ねることで変数の内容に応じた出力がされる

--- sec2-1.php
+++ sec2-1.php
@@ -1,4 +1,23 @@
 <?php
+
 $msg = 'こんにちは！';
 print $msg;
+
+$x = "title";
+$title = "PHP:Hypertext Preprocessor";
+
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>sec2-2</title>
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+    <p style="color: red;"><?php print $$x ?></p>
+</body>
+</html>
